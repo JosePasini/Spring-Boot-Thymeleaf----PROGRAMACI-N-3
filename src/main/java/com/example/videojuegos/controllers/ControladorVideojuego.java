700cfbe57745df61bf6420d2527d4c19f627766b
JosePasini <josepasini.17@gmail.com>
2021-09-24 20:27:37 -0300

    imagenes

--- src/main/java/com/example/videojuegos/controllers/ControladorVideojuego.java
+++ src/main/java/com/example/videojuegos/controllers/ControladorVideojuego.java
@@ -3,11 +3,11 @@ package com.example.videojuegos.controllers;
 import com.example.videojuegos.entities.Videojuego;
 import com.example.videojuegos.services.VideojuegoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -18,25 +18,27 @@ public class ControladorVideojuego {
     private VideojuegoService videojuegoService;
 
 
-    @GetMapping(value = "/getAll")
-    public ResponseEntity<?> getAll(){
-        try {
-            return ResponseEntity.status(HttpStatus.OK).body(this.videojuegoService.findAll());
-        } catch (Exception e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error dafdsa");
-        }
-    }
-
     @GetMapping(value = "/inicio")
     public String inicio(Model model){
         try{
             List<Videojuego> videojuegoList = this.videojuegoService.findAllByActivo();
             model.addAttribute("lista_videojuegos", videojuegoList);
             return "views/inicio";
-
         } catch (Exception e){
-            return "";
+            model.addAttribute("error", e.getMessage());
+            return "error";
         }
     }
 
+    @GetMapping(value = "/detalle/{id}")
+    public String detalleVideojuego(Model model, @PathVariable("id") Long id) throws Exception {
+        try {
+            Videojuego videojuego = this.videojuegoService.findById(id);
+            model.addAttribute("videojuego", videojuego);
+            return "views/detalle";
+        }catch (Exception e){
+            model.addAttribute("error", e.getMessage());
+            return "error";
+        }
+    }
 }
